7b149ada74e517acf56a753ca4b3e4c5327ddfbe
bgreni <grenierb96@gmail.com>
2021-01-17 16:54:55 -0700

    done Q4

--- A1/1545276.lisp
+++ A1/1545276.lisp
@@ -19,6 +19,8 @@ NIL will be returned
 )
 
 #| QUESTION 2
+unests lists inside of a list
+
 I don't fucking know
 |#
 (defun flatten (x)
@@ -30,7 +32,7 @@ I don't fucking know
 )
 
 #| QUESTION 3
-
+ Removes duplicate values from a list
 |#
 (defun remove-duplicate (x)
     (cond 
@@ -40,11 +42,34 @@ I don't fucking know
     )
 )
 
+#| QUESTION 4
 
+|#
+(defun mix (L1 L2)
+    (cond 
+        ((null L1) L2)
+        (T (cons (car L1) (mix L2 (cdr L1))))
+    )
+)
+
+#| QUESTION 5
+|#
+(defun allsubsets (L)
+     (gen-subsets (cons nil nil) L)
+)
+
+(defun gen-subsets (L1 L2)
+    (cond
+        ((null L1) L2)
+        (T (cons (car L1) (gen-subsets (cdr L1) L2)))
+    )
+)
 
 ;; (print (xmember NIL '(NIL) ))
 ;; (print (flatten '(1 2 (2))))
-(print (remove-duplicate '(a b c a d b)))
+;; (print (remove-duplicate '(a b c a d b)))
+;; (print (mix'(1 2 3) nil))
+(print (allsubsets '(a b)))
 (terpri)
     
     
