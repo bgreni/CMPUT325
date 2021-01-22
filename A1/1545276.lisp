a779d306cb9c367a7d8271ddca794d48d84b1dc9
bgreni <grenierb96@gmail.com>
2021-01-21 17:20:12 -0700

    added comments and fixed referTo

--- A1/1545276.lisp
+++ A1/1545276.lisp
@@ -19,9 +19,14 @@ NIL will be returned
 )
 
 #| QUESTION 2
-unests lists inside of a list
+This function unests any list inside of a list x
 
-I don't fucking know
+Test case:
+(flatten '(a (b c))) => (a b c)
+
+If checks if the list x is empty, and then return NIL,
+then if the first item is an atom, that atom is cons'd with the flattened version of the rest of the list through recursion
+otherwise, the flattened version of the first in the list is appended with the flattened version of the rest of the list
 |#
 (defun flatten (x)
     (cond 
@@ -32,7 +37,11 @@ I don't fucking know
 )
 
 #| QUESTION 3
- Removes duplicate values from a list
+Removes duplicate values from a list
+
+checks if list x is empty and returns NIL
+then checks if the first item in the list is a member of the rest of the list, then makes a recursive call exlcuding the rest of the list
+otherwise combines the first item in the list, with the de-duplicated version of the rest of the list using recursion
 |#
 (defun remove-duplicate (x)
     (cond 
@@ -43,11 +52,18 @@ I don't fucking know
 )
 
 #| QUESTION 4
+Combines two lists by choosing elements from L1 and L2 alternatingly. If one of the lists are emtpy, then the rest of the
+longer list is added
 
+Checks if L1 is null, then returns the rest of L2
+Checks if L2 is null, then returns the rest of L1
+otherwise combines the first element of L1 with the rest of the mixed list using recursion, swapping the position of the arguments
+L1 and L2, so that on the next recusive call, the first item in L2 will be choosen
 |#
 (defun mix (L1 L2)
     (cond 
         ((null L1) L2)
+        ((null L2) L1)
         (T (cons (car L1) (mix L2 (cdr L1))))
     )
 )
@@ -72,9 +88,19 @@ I don't fucking know
 )
 
 #| QUESTION 6
+Contains definitions for both the reached and ranked functions
 |#
 
 (defun reached (x L)
+"
+Given and atom x and a list of pairs (a b), returns all items b that are paired with a, such that a == x
+In the context of the question, returns all sites b, that can be reached from site x
+Testcase: (reached 1 '((2 3) (1 3) (2 1))) => (3)
+
+If L is empty returns NIL
+Then checks if x is equal to the first element in the first pair of L, then adds the second item in the pair to the list
+otherwise cuts off that pair in a recursive call
+"
     (cond 
        ((null L) NIL)
        ((equal x (caar L)) (cons (cadar L) (reached x (cdr L))))
@@ -84,6 +110,10 @@ I don't fucking know
 
 
 (defun depair (L)
+"
+Given a list of pairs (a b), returns a list containing only the first value in each pair
+Testcase: (depair '((1 2))) => (1)
+"
     (cond
         ((null L) NIL)
         (T (cons (caar L) (depair (cdr L))))
@@ -91,46 +121,74 @@ I don't fucking know
 )
 
 (defun rank (S L)
+"
+Prints the list of elemets in S sorted by how many times they appear as B in a list of pairs (A B) L
+In the context of the question, prints S ordered in in terms of importance
+
+Testcase: (rank '(a b c d) '((a b) (a c) (b c) (d a) (d b))) => (B C A D)
+
+Works by compiling a list of pairs (A B) where A is a member of S and B is its importance value
+The list is then sorted in increasing order by importance value of each pair
+The importance value is then removed from the pairs and the list of members of S is returned
+"
     (depair (mySort (compileFrequencies S L)))
 )
 
 (defun len (L)
+"
+Returns the length of a list L
+"
     (if (null L) 0
         (+ 1 (len (cdr L)))
     )
 )
 
-(defun referTo (x L)
+(defun referTo (x L seen)
+"
+Creates a list of elements from a list of pairs L (A B), where B == X, compiling a list of A where such is true
+Testcase: (referTo 3 '((1 2) (1 3) (2 3))) => (1 2)
+"
     (cond
         ((null L) NIL)
-        ((equal x (cadar L)) (cons (caar L) (referTo x (cdr L))))
-        (T (referTo x (cdr L)))
+        ((xmember (car L) seen) (referTo x (cdr L) seen))
+        ((equal x (cadar L)) (cons (caar L) (referTo x (cdr L) (cons (car L) seen))))
+        (T (referTo x (cdr L) seen))
     )
 )
 
 (defun compileFrequencies (S L)
+"
+Goes through the elements in a list S, and compiles the number of times it appears as B in a list of pairs (A B)
+Testcase: (compileFrequencies '(a b c d) '((a b) (a c) (b c) (d a))) => ((A . 1) (B . 1) (C . 2) (D . 0))
+"
     (cond
        ((null S) NIL)    
-       (T (cons (cons (car S) (len (referTo (car S) L))) (compileFrequencies (cdr S) L)))
+       (T (cons (cons (car S) (len (referTo (car S) L NIL))) (compileFrequencies (cdr S) L)))
     )
 )
 
 (defun mySort (L)
+"
+sorts a list of pair (A B) where B is an integer in increasing order by B
+"
     (sort L 'compareFunc)
 )
 
 (defun compareFunc (X Y)
+"
+Given two pairs X and Y of the form (A B), returns true of X.B > Y.B
+"
     (> (cdr X) (cdr Y))
 )
 
 ;; (print (xmember NIL '(NIL) ))
 ;; (print (flatten '(1 2 (2))))
 ;; (print (remove-duplicate '(a b c a d b)))
-;; (print (mix'(1 2 3) '(4 5 6)))
+;; (print (mix'(1 3 5) '(2 4 6)))
 ;; (print (allsubsets '(a b)))
-;; (print (reached 1 '((2 3) (1 3) (2 3))))
+;; (print (reached 1 '((2 3) (1 3) (2 1))))
 ;; (print (len '(1 2 3 7)))
-;; (print (compileFrequencies '(a b c d) '((a b) (a c) (b c) (d a))))
+;; (print (compileFrequencies '(a b c d) '((a b) (a c) (b c) (d a) (a b) (c b))))
 ;;  (print (referTo 3 '((1 2) (1 3) (2 3))))
 ;; (print (equal 3 (cadar '((2 3)))))
 ;; (print (mySort '((a 2) (b 9) (s 4))))
